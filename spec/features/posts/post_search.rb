073105f9d37641bf3e1f6709c8dc90180476ab4f
Vadim <kurnatovskiyvadim@gmail.com>
2018-09-04 15:16:07 +0300

    spec

--- spec/features/posts/post_search.rb
+++ spec/features/posts/post_search.rb
@@ -1,40 +1,23 @@
-RSpec.describe FindPosts do
-  let(:initial_scope) { Post.all }
+require "rails_helper"
 
-  let(:search) { string 'some' }
-  let(:empty_params) { {} }
-  let(:params) { [:search, :order, :page] }
-  empty_subject { described_class.new(initial_scope).call(empty_params) }
-  subject { described_class.new(initial_scope).call(params)}
+feature "As user I can search posts" do
 
-  context 'with empty params' do
-
-    it 'search' do
-      expect(empty_subject.to_sql).to include('SELECT  "posts".* FROM "posts" WHERE (title LIKE %% OR content SIMILAR TO %%)')
-    end
-
-    it 'order' do
-      expect(empty_subject.to_sql).to include('ORDER BY "posts"."created_at" DESC')
-    end
-
-    it 'paginates' do
-      expect(empty_subject.to_sql).to include('LIMIT')
-      expect(empty_subject.to_sql).to include('OFFSET')
-    end
+  scenario "with title query parameters" do
+    fill_in :search, with: "MyString"
+    click_button "Search"
+    expect(page).to have_content "MyString"
   end
-  context "with parameters" do
 
-    it 'search' do
-      expect(subject.to_sql).to include('SELECT  "posts".* FROM "posts" WHERE (title LIKE some OR content SIMILAR TO some)')
-    end
-
-    it 'order' do
-      expect(subject.to_sql).to include('ORDER BY "posts"."created_at" DESC')
-    end
-
-    it 'paginates' do
-      expect(subject.to_sql).to include('LIMIT')
-      expect(subject.to_sql).to include('OFFSET')
-    end
+  scenario "with description query parameters" do
+    fill_in :search, with: "Some"
+    click_button "Search"
+    expect(page).to have_content "Some"
   end
+
+  scenario "with empty parameters" do
+    fill_in :search, with: ""
+    click_button "Create Course"
+    expect(page).to have_content "No results"
+  end
+
 end
