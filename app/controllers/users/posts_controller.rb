027034c391fc8aa3cbe0656a4063b5602dc73ecf
VadimKurnatovskiy <kurnatovskiyvadim@gmail.com>
2018-09-04 17:51:22 +0300

    update users posts controller
    refactor

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -1,16 +1,13 @@
 module Users
   class PostsController < ApplicationController
     before_action :authenticate_user!
-    expose_decorated(:post, attributes: :post_params)
+    expose_decorated(:post)
     expose_decorated(:comments) { post.comments.includes(:user) }
 
     def create
       post.user = current_user
-      if post.save
-        respond_with post
-      else
-        render "new"
-      end
+      post.save
+      respond_with post
     end
 
     def destroy
@@ -27,12 +24,6 @@ module Users
     end
 
     def edit
-    end
-
-    private
-
-    def post_params
-      params.require(:post).permit(:title, :content)
     end
   end
 end
